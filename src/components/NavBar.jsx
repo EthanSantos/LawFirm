e44c373d0f30e26d8b573111b41c1735ece35d78
EthanSantos <ethanangelo61@gmail.com>
2024-09-02 13:42:35 -0700

    logo and updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import logo from '../assets/GMSLogo.png'; // Adjust the path as needed
 
 const NavBar = () => {
     const navbarRef = useRef(null);
-    const dropdownRef = useRef(null);
+    const immigrationDropdownRef = useRef(null);
+    const bankruptcyDropdownRef = useRef(null);
     const location = useLocation();
     const navigate = useNavigate();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isImmigrationDropdownOpen, setIsImmigrationDropdownOpen] = useState(false);
+    const [isBankruptcyDropdownOpen, setIsBankruptcyDropdownOpen] = useState(false);
 
     useEffect(() => {
         const handleHashChange = () => {
@@ -27,8 +30,11 @@ const NavBar = () => {
         window.addEventListener('hashchange', handleHashChange);
 
         const handleClickOutside = (event) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-                setIsDropdownOpen(false);
+            if (immigrationDropdownRef.current && !immigrationDropdownRef.current.contains(event.target)) {
+                setIsImmigrationDropdownOpen(false);
+            }
+            if (bankruptcyDropdownRef.current && !bankruptcyDropdownRef.current.contains(event.target)) {
+                setIsBankruptcyDropdownOpen(false);
             }
         };
 
@@ -43,21 +49,38 @@ const NavBar = () => {
     const handleImmigrationClick = (e) => {
         if (e.target === e.currentTarget) {
             navigate('/immigration');
-            setIsDropdownOpen(false);
+            setIsImmigrationDropdownOpen(false);
         }
     };
 
-    const toggleDropdown = (e) => {
+    const handleBankruptcyClick = (e) => {
+        if (e.target === e.currentTarget) {
+            navigate('/bankruptcy');
+            setIsBankruptcyDropdownOpen(false);
+        }
+    };
+
+    const toggleImmigrationDropdown = (e) => {
         e.stopPropagation();
-        setIsDropdownOpen(!isDropdownOpen);
+        setIsImmigrationDropdownOpen(!isImmigrationDropdownOpen);
+    };
+
+    const toggleBankruptcyDropdown = (e) => {
+        e.stopPropagation();
+        setIsBankruptcyDropdownOpen(!isBankruptcyDropdownOpen);
     };
 
     return (
         <div className="bg-white fixed w-full z-50 top-0" ref={navbarRef}>
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center py-4">
-                    <div className="text-lg text-blue-600 font-bold mb-2">
-                        <Link to="/#home" className="hover:text-blue-700">
+                    <div className="flex items-center">
+                        <img
+                            src={logo}
+                            alt="Logo"
+                            className="w-12 h-12 sm:w-16 sm:h-16 mr-4"
+                        />
+                        <Link to="/#home" className="text-xl sm:text-2xl text-blue-600 font-bold hover:text-blue-700">
                             LAW OFFICES OF GWENDOLYN M. SANTOS
                         </Link>
                     </div>
@@ -68,14 +91,14 @@ const NavBar = () => {
                     </div>
                     <div className={`absolute w-full sm:static sm:w-auto bg-white sm:bg-transparent top-16 right-0 sm:flex flex-col sm:flex-row ${isMenuOpen ? 'flex' : 'hidden'} space-y-2 sm:space-y-0 sm:space-x-4 p-4 sm:p-0`}>
                         <Link to="/testimonials" onClick={() => setIsMenuOpen(false)} className="text-gray-800 hover:text-blue-600 text-sm font-medium">Testimonials</Link>
-                        <div className="relative" ref={dropdownRef}>
+                        <div className="relative" ref={immigrationDropdownRef}>
                             <button
                                 onClick={handleImmigrationClick}
                                 className="text-gray-800 hover:text-blue-600 text-sm font-medium flex items-center"
                             >
                                 Immigration
                                 <svg
-                                    onClick={toggleDropdown}
+                                    onClick={toggleImmigrationDropdown}
                                     className="w-4 h-4 ml-1 cursor-pointer"
                                     fill="none"
                                     stroke="currentColor"
@@ -85,15 +108,39 @@ const NavBar = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
                                 </svg>
                             </button>
-                            {isDropdownOpen && (
+                            {isImmigrationDropdownOpen && (
                                 <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-                                    <Link to="/immigration/test" onClick={() => { setIsMenuOpen(false); setIsDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Naturilization/Citizenship</Link>
-                                    <Link to="/immigration/test" onClick={() => { setIsMenuOpen(false); setIsDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Family-Based Immigration Petition</Link>
-                                    <Link to="/immigration/test" onClick={() => { setIsMenuOpen(false); setIsDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Fiance Visa</Link>
+                                    <Link to="/immigration/naturalization" onClick={() => { setIsMenuOpen(false); setIsImmigrationDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Naturalization/Citizenship</Link>
+                                    <Link to="/immigration/family-based" onClick={() => { setIsMenuOpen(false); setIsImmigrationDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Family-Based Immigration Petition</Link>
+                                    <Link to="/immigration/fiance-visa" onClick={() => { setIsMenuOpen(false); setIsImmigrationDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Fiance Visa</Link>
                                 </div>
                             )}
                         </div>
-                        <Link to="/bankruptcy" onClick={() => setIsMenuOpen(false)} className="text-gray-800 hover:text-blue-600 text-sm font-medium">Bankruptcy</Link>
+                        <div className="relative" ref={bankruptcyDropdownRef}>
+                            <button
+                                onClick={handleBankruptcyClick}
+                                className="text-gray-800 hover:text-blue-600 text-sm font-medium flex items-center"
+                            >
+                                Bankruptcy
+                                <svg
+                                    onClick={toggleBankruptcyDropdown}
+                                    className="w-4 h-4 ml-1 cursor-pointer"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                                </svg>
+                            </button>
+                            {isBankruptcyDropdownOpen && (
+                                <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+                                    <Link to="/bankruptcy/chapter-7" onClick={() => { setIsMenuOpen(false); setIsBankruptcyDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Chapter 7 Bankruptcy Petition</Link>
+                                    <Link to="/bankruptcy/chapter-13" onClick={() => { setIsMenuOpen(false); setIsBankruptcyDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Chapter 13 Bankruptcy Petition</Link>
+                                    <Link to="/bankruptcy/faq" onClick={() => { setIsMenuOpen(false); setIsBankruptcyDropdownOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">FAQ On Bankruptcy</Link>
+                                </div>
+                            )}
+                        </div>
                         <Link to="/blog" onClick={() => setIsMenuOpen(false)} className="text-gray-800 hover:text-blue-600 text-sm font-medium">Our Blog</Link>
                         <Link to="/publications" onClick={() => setIsMenuOpen(false)} className="text-gray-800 hover:text-blue-600 text-sm font-medium">Publications</Link>
                         <Link to="/#about-us" onClick={() => setIsMenuOpen(false)} className="text-gray-800 hover:text-blue-600 text-sm font-medium">About Us</Link>
